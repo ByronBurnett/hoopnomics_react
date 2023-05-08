efd75ecdd759dbf0d293ffa2e30fbdb13f439883
Byron Burnett <byron.burnett@outlook.com>
2023-05-08 13:58:28 +0300

    update backend code

--- backend/server.js
+++ backend/server.js
@@ -66,10 +66,6 @@ const mongoose = require('mongoose')
 //Middleware
 app2.use(express.json())
 app2.use(cors({credentials:true,origin:'https://hoopnomics.com'}));         
-app2.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-})
 app2.use('/uploads', express.static(__dirname + '/uploads'));
 app2.use(cookieParser());
 
