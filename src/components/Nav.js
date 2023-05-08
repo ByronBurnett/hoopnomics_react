75efd743f5c60a792c8699bb1e0006312a1a85df
Byron Burnett <byron.burnett@outlook.com>
2023-05-08 18:45:33 +0300

    naw

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -18,7 +18,7 @@ const {setUserInfo, userInfo} = useContext(UserContext);
 
  
    useEffect(() => {
-      fetch('https://hoopnomics.com/profile',  {
+      fetch('https://mern-crud-ued0.onrender.com/profile',  {
        credentials: 'include',
    
       }).then(response => {
